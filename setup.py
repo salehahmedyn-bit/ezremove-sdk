c60ece0c6aaea87cf4acda137c763094f1c3e2bc
root <root@salahahmedyn.com>
2026-01-01 17:31:14 +0000

    Final: Secured and encrypted SDK with local runtime

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name="salah-ahmedyn",
     version="0.1.0",
-    packages=find_packages(),
+    packages=[
+        "ezremove",
+        "ezremove.pyarmor_runtime_000000"
+    ],
+    package_data={
+        "ezremove.pyarmor_runtime_000000": ["*.so", "*.py"],
+    },
+    include_package_data=True,
     install_requires=[
         "aiohttp>=3.8.0",
     ],
-    author="Salah Ahmed",
-    description="Professional Async SDK for EzRemove API",
-    python_requires=">=3.7",
 )
